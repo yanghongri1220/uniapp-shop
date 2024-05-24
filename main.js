2c2bd8366fb118e27a73bb40e8aeb5d463095d13
yanghongri <1797427456@qq.com>
2024-05-24 18:11:08 +0800

    home首页完结

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+
+
+import {
+	$http
+} from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,41 @@
 <template>
 	<view>
-		
+		<!-- 轮播区 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="item,i in swiperlist" :key="i">
+				<navigator :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id" class="swiper-item">
+					<image :src="item.image_src"></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<!-- 分类导航区 -->
+		<view class="nav-box">
+			<view @click="navJump(item)" class="nav-item" v-for="item,i in navList" :key="i">
+				<image class="nav-image" :src="item.image_src" mode=""></image>
+			</view>
+		</view>
+		<!-- 楼层区 -->
+		<view class="floor-box">
+			<view class="floor-item" v-for="item,i in floorList" :key="i">
+				<view class="floor-title">
+					<image :src="item.floor_title.image_src" mode=""></image>
+				</view>
+				<view class="floor-content">
+					<navigator class="content-left" :url="item.product_list[0].url">
+						<image :style="{width:item.product_list[0].image_width+'rpx'}"
+							:src="item.product_list[0].image_src" mode="widthFix"></image>
+					</navigator>
+					<view class="content-right">
+						<navigator v-for="item1,i1 in item.product_list" :url="item1.url" class="" v-if="
+							i1!=0" :key="i1">
+							<image v-if="i1!=0" :style="{width:item1.image_width+'rpx'}" :src="item1.image_src"
+								mode="widthFix">
+							</image>
+						</navigator>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +43,91 @@
 	export default {
 		data() {
 			return {
-				
+				swiperlist: [],
+				navList: [],
+				floorList: [],
 			};
+		},
+		methods: {
+			async getSwiperList() {
+				const res = await uni.$http.get('/api/public/v1/home/swiperdata')
+				this.swiperlist = res.data.message
+			},
+			async getNavList() {
+				const res = await uni.$http.get('/api/public/v1/home/catitems')
+				this.navList = res.data.message
+			},
+			async getFloorList() {
+				const res = await uni.$http.get('/api/public/v1/home/floordata')
+				this.floorList = res.data.message
+				this.floorList.map(item => {
+					item.product_list.map(item1 => {
+						item1.url = '/subpkg/goods_list/goods_list?' + item1.navigator_url.split("?")[1]
+					})
+				})
+				console.log("楼层区域", res.data.message)
+			},
+			navJump(item) {
+				if (item.name == '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			}
+		},
+		onLoad() {
+			this.getSwiperList()
+			this.getNavList()
+			this.getFloorList()
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.nav-box {
+		display: flex;
+		margin: 10rpx;
+		justify-content: space-between;
+
+		.nav-image {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	.floor-box {
+		.floor-item {
+			.floor-title {
+				margin-top: 15rpx;
+
+				image {
+					width: 100%;
+					height: 60rpx;
+				}
+			}
+
+			.floor-content {
+				display: flex;
+
+				.content-right {
+					margin-left: 10rpx;
+					display: flex;
+					flex-wrap: wrap;
+					justify-content: space-around;
+				}
+			}
+		}
+
+
+	}
 </style>
